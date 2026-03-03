a0b7a194cbeca39da3ef17a2a8564462a4fe8dff
vkESCai <122730374+vkESCai@users.noreply.github.com>
2026-03-04 01:23:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,12 @@ app.get("/", (req, res) => {
   res.send("API Working")
 });
 
+app.use(cors({
+  origin: [
+    "https://frontend-doc-ten.vercel.app",
+    "https://admin-care-connect-sigma.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
